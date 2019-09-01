df19cb9ba64e577786e291f7e82e1173463f3ed0
neal.nankivell <neal.nankivell@gmail.com>
2019-09-01 11:35:40 +0200

    Added template solution form leetcode

---
+++ Answer/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Answer
+{
+    public class Solution
+    {
+        /*
+        Given an array of integers, find if the array contains any duplicates.
+
+        Your function should return true if any value appears at least twice in
+        the array, and it should return false if every element is distinct.
+         */
+        public bool ContainsDuplicate(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
